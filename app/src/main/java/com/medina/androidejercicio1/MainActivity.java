e6d1725a84216c8529a50f32410934a0c26e0263
Victor <vmedina@ucol.mx>
2017-10-23 18:03:55 -0500

    Ejercicio 1 Android

---
+++ app/src/main/java/com/medina/androidejercicio1/MainActivity.java
@@ -0,0 +1,48 @@
+package com.medina.androidejercicio1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1,et2;
+    private TextView tv3;
+    private RadioButton r1,r2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.et1);
+        et2=(EditText)findViewById(R.id.et2);
+        tv3=(TextView)findViewById(R.id.tv3);
+        r1=(RadioButton)findViewById(R.id.r1);
+        r2=(RadioButton)findViewById(R.id.r2);
+    }
+
+
+
+    //Este método se ejecutará cuando se presione el botón
+    public void operar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        if (r1.isChecked()==true) {
+            int suma=nro1+nro2;
+            String resu=String.valueOf(suma);
+            tv3.setText(resu);
+        } else
+        if (r2.isChecked()==true) {
+            int resta=nro1-nro2;
+            String resu=String.valueOf(resta);
+            tv3.setText(resu);
+        }
+    }
+}
